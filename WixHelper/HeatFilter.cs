950b25e3a5dfc577ed5124c8a3bbcfd988289633
the-nerbs <scott.nerbetski@gmail.com>
2021-07-29 16:07:29 -0400

    Added new PDBs to the installer.
    They were technically being included already thanks to the third-party DLL scan, but that was just a coincidence. This commit adds a name filter to the WixHelper, and applies that  to the third party scan to avoid picking up the HexEdit files.

--- WixHelper/HeatFilter.cs
+++ WixHelper/HeatFilter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml.Linq;
 
@@ -21,16 +22,18 @@ namespace WixHelper
             {
                 ExtensionFilter(extArg.Value!, document);
             }
+
+            if (arguments.NamedArguments.TryGetValue("name", out var nameArg))
+            {
+                NameFilter(nameArg.Value!, document);
+            }
         }
 
-        private void ExtensionFilter(string exts, XDocument wix)
+        private static void ExtensionFilter(string exts, XDocument wix)
         {
             var allowedExtensions = new HashSet<string>(exts.Split(';'), StringComparer.OrdinalIgnoreCase);
 
-            var compsParent = GetComponentsParent(wix);
-            var compGroup = GetComponentRefsParent(wix);
-
-            foreach (XElement comp in compsParent.Elements().ToArray())
+            foreach (XElement comp in GetComponentsParent(wix).Elements().ToArray())
             {
                 if (comp.Element(WixNames.File) is XElement file)
                 {
@@ -40,16 +43,84 @@ namespace WixHelper
                     if (string.IsNullOrEmpty(sourceExt)
                         || !allowedExtensions.Contains(sourceExt))
                     {
-                        string id = comp.Attribute(WixNames.Id).Value;
-
-                        comp.Remove();
-
-                        foreach (var reference in compGroup.Elements().Where(e => e.Attribute(WixNames.Id).Value == id))
-                        {
-                            reference.Remove();
-                        }
+                        RemoveComponent(wix, comp);
                     }
                 }
+            }
+        }
+
+        private static void NameFilter(string filterList, XDocument wix)
+        {
+            ReadFilterList(filterList, out List<Regex> include, out List<Regex> exclude);
+
+            foreach (XElement comp in GetComponentsParent(wix).Elements().ToArray())
+            {
+                if (comp.Element(WixNames.File) is XElement file)
+                {
+                    string source = file.Attribute(WixNames.Source).Value;
+                    string sourceName = Path.GetFileName(source);
+
+                    // include if:
+                    //  there are no inclusion patterns, or
+                    //  any of the inclusion patterns match the filename
+                    bool includeComp = include.Count == 0
+                                    || include.Any(p => p.IsMatch(sourceName));
+
+                    // exclude if:
+                    //  we should not include it, or
+                    //  an exclude pattern does match
+                    //
+                    // note that this gives precedence to the exclusions.
+                    if (!includeComp
+                        || exclude.Any(p => p.IsMatch(sourceName)))
+                    {
+                        RemoveComponent(wix, comp);
+                    }
+                }
+            }
+        }
+
+
+        private static void ReadFilterList(string arg, out List<Regex> include, out List<Regex> exclude)
+        {
+            string[] patterns = arg.Split(';');
+
+            include = new List<Regex>();
+            exclude = new List<Regex>();
+
+            foreach (string item in patterns)
+            {
+                string trimmed = item.Trim();
+                string pattern;
+                List<Regex> collection;
+
+                if (trimmed.StartsWith("!"))
+                {
+                    pattern = trimmed.Substring(1).TrimStart();
+                    collection = exclude;
+                }
+                else
+                {
+                    pattern = trimmed;
+                    collection = include;
+                }
+
+                collection.Add(new Regex(pattern));
+            }
+        }
+
+        private static void RemoveComponent(XDocument wix, XElement comp)
+        {
+            string id = comp.Attribute(WixNames.Id).Value;
+
+            // remove the Component from the Directory/DirectoryRef
+            comp.Remove();
+
+            // remove the ComponentRef from the ComponentGroup
+            var compGroup = GetComponentRefsParent(wix);
+            foreach (var reference in compGroup.Elements().Where(e => e.Attribute(WixNames.Id).Value == id))
+            {
+                reference.Remove();
             }
         }
     }
