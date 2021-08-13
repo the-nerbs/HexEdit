92bbd2b0e26c4bd3e3bc9280cce3840143e059e9
the-nerbs <scott.nerbetski@gmail.com>
2021-08-12 20:09:43 -0400

    Updates for new html help paths.
    - Updated paths to the html doc files in installer and MakehelpV.bat
    - Deleted Makehelp.bat and MakeHelpMap.bat. These appear to have been to support older help builds, and MakeHelpV.bat was the only one in use from the modern vcxproj when I forked the repo.

--- HexEdit/MakehelpV.bat
+++ HexEdit/MakehelpV.bat
@@ -8,27 +8,30 @@ REM Work out location of sed (assumes Git is installed)
 set SEDCMD=sed
 IF EXIST "C:\Program Files (x86)\SmartGit\git\bin\sed.exe" set SEDCMD=C:\Program Files (x86)\SmartGit\git\bin\sed.exe
 IF EXIST "C:\Program Files (x86)\Git\bin\sed.exe"          set SEDCMD=C:\Program Files (x86)\Git\bin\sed.exe
+IF EXIST "C:\Program Files\Git\usr\bin\sed.exe"            set SEDCMD=C:\Program Files\Git\usr\bin\sed.exe
+
+set HTMLHELP=..\docs\HTMLHelp
 
 ECHO Command IDs
-echo // MAKEHELP.BAT generated Help Map file.  >"HTMLHELP\CmdIdMap.h"
-echo // Commands (ID_* and IDM_*)  >>"HTMLHELP\CmdIdMap.h"
+echo // MAKEHELP.BAT generated Help Map file.  >"%HTMLHELP%\CmdIdMap.h"
+echo // Commands (ID_* and IDM_*)  >>"%HTMLHELP%\CmdIdMap.h"
 makehm  ID_,HID_,0x10000 IDM_,HIDM_,0x10000 resource.h  >"CmdIdMap.tmp"
-"%SEDCMD%"  "s/^HID/#define HID/"  <"CmdIdMap.tmp"  >>"HTMLHELP\CmdIdMap.h"
+"%SEDCMD%"  "s/^HID/#define HID/"  <"CmdIdMap.tmp"  >>"%HTMLHELP%\CmdIdMap.h"
 del "CmdIdMap.tmp"
 
 ECHO MFC Command IDs
-copy /y HTMLHelp\afxhelp.hm  AfxIdMap.tmp
+copy /y "%HTMLHELP%\afxhelp.hm"  AfxIdMap.tmp
 "%SEDCMD%"  "s/^HID/#define HID/"  <AfxIdMap.tmp  >AfxId2.tmp
-"%SEDCMD%"  "s/^AFX_HID/#define AFX_HID/"  <AfxId2.tmp  >"HTMLHELP\AfxIdMap.h"
+"%SEDCMD%"  "s/^AFX_HID/#define AFX_HID/"  <AfxId2.tmp  >"%HTMLHELP%\AfxIdMap.h"
 del AfxIdMap.tmp
 del AfxId2.tmp
 
 ECHO Dialog IDs
-echo // MAKEHELP.BAT generated Help Map file.  >"HTMLHELP\DlgIdMap.h"
-echo // Dialogs (IDD_*)  >>"HTMLHELP\DlgIdMap.h"
+echo // MAKEHELP.BAT generated Help Map file.  >"%HTMLHELP%\DlgIdMap.h"
+echo // Dialogs (IDD_*)  >>"%HTMLHELP%\DlgIdMap.h"
 makehm  IDD_,HIDD_,0x20000 resource.h  >"DlgIdMap.tmp"
-"%SEDCMD%"  "s/^HID/#define HID/"  <"DlgIdMap.tmp"  >>"HTMLHELP\DlgIdMap.h"
+"%SEDCMD%"  "s/^HID/#define HID/"  <"DlgIdMap.tmp"  >>"%HTMLHELP%\DlgIdMap.h"
 del "DlgIdMap.tmp"
 
 ECHO Control IDs (for What's This help)
-copy /y helpid.hm+resource.hm  HTMLHelp\CtlIdMap.h
+copy /y helpid.hm+resource.hm  "%HTMLHELP%\CtlIdMap.h"
